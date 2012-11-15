c14110b922ad4e661e6f0e587bc27c043554a10b
lkorth <luke@lukekorth.com>
2012-11-14 23:01:34 -0600

    Load posts from database to pass on to template

--- sections/blog.php
+++ sections/blog.php
@@ -4,6 +4,8 @@ $app->get('/blog', function () use ($app) {
     $arr = array();
     $arr['title'] = 'Blog :: LukeKorth.com';
     $arr['page'] = 'blog';
+    $arr['posts'] = R::findAll('post', ' ORDER BY date DESC LIMIT 3 ');
+    R::preload($arr['posts'], array('author'));
 
     $app->render('blog.twig', $arr);
 });
