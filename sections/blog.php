f8c0a77a0da796d3a6daafd48f284afa8b9194fb
lkorth <luke@lukekorth.com>
2012-11-17 19:38:10 -0600

    Add optional page to blog route

--- sections/blog.php
+++ sections/blog.php
@@ -1,10 +1,13 @@
 <?php
 
-$app->get('/blog', function () use ($app) {
+$app->get('/blog(/:page)', function ($page = 0) use ($app) {
+    $first = $page * 3;
+    $count = 3;
+
     $arr = array();
     $arr['title'] = 'Blog :: LukeKorth.com';
     $arr['page'] = 'blog';
-    $arr['posts'] = R::findAll('post', ' ORDER BY date DESC LIMIT 3 ');
+    $arr['posts'] = R::findAll('post', ' ORDER BY date DESC LIMIT ?,? ', array($first, $count));
     R::preload($arr['posts'], array('author'));
 
     $app->render('blog.twig', $arr);
