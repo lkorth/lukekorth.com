b402127475b0722caafee2baeed69d6fd288d637
lkorth <luke@lukekorth.com>
2013-01-15 10:03:06 -0600

    Enable simple cache in RedBean and add production conditional for freezing database schema and enabling template caching

--- index.php
+++ index.php
@@ -8,13 +8,22 @@ require('helpers/functions.php');
 // setup db connection
 class R extends RedBean_Facade {}
 R::setup('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
+R::$writer->setUseCache(true);
+if(PRODUCTION) {
+	R::freeze();
 
-// cache dir needs to be writable by server
-\Slim\Extras\Views\Twig::$twigTemplateDirs = array('templates');
-\Slim\Extras\Views\Twig::$twigOptions = array(
-	'charset'           => 'utf-8',
-	'cache'             => 'cache'
-);
+	// cache dir needs to be writable by server
+	\Slim\Extras\Views\Twig::$twigTemplateDirs = array('templates');
+	\Slim\Extras\Views\Twig::$twigOptions = array(
+		'charset'           => 'utf-8',
+		'cache'             => 'cache'
+	);
+} else {
+	\Slim\Extras\Views\Twig::$twigTemplateDirs = array('templates');
+	\Slim\Extras\Views\Twig::$twigOptions = array(
+		'charset'           => 'utf-8'
+	);
+}
 
 $pjax = false;
 $headers = getallheaders();
