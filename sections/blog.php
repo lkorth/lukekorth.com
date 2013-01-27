d85841e68a3df94a6f6365908a6e99024351f68d
lkorth <luke@lukekorth.com>
2013-01-27 00:38:03 -0600

    Fix paging for categories

--- sections/blog.php
+++ sections/blog.php
@@ -5,6 +5,8 @@ $app->get('/blog(/:page)', function ($page = 0) use ($app) {
     $arr['title'] = 'Blog :: LukeKorth.com';
     $arr['page']['number'] = $page;
     $arr['page']['name'] = 'blog';
+    $arr['link'] = '';
+
     $arr['posts'] = R::findAll('post', ' ORDER BY date DESC LIMIT ?,? ', array($page * 3, 3));
     R::preload($arr['posts'], array('author'));
     if (R::count('post') > ($page * 3) + 3)
@@ -27,6 +29,7 @@ $app->get('/blog/category/:category(/:page)', function($category, $page = 0) use
     $arr['title'] = 'Blog :: LukeKorth.com';
     $arr['page']['number'] = $page;
     $arr['page']['name'] = 'blog';
+    $arr['link'] = '/category/' . $category;
 
     $arr['posts'] = R::tagged('post', array($category));
     usort($arr['posts'], 'orderPosts');
