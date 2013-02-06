d9680f07f51b2fd2f67352e1b9903351bb5a7105
lkorth <luke@lukekorth.com>
2013-02-05 21:31:11 -0600

    Update fake route to generate comments

--- sections/faker.php
+++ sections/faker.php
@@ -1,13 +1,14 @@
 <?php
 
 $app->get('/fake', function () use ($app) {
-    // use the factory to create a Faker\Generator instance
+    /*
     $faker = Faker\Factory::create();
 
     $author = R::dispense('author');
     $author->name = 'Luke';
     $author->username = strtolower($author->name);
     $author->password = $faker->md5;
+    $author->id = R::store($author);
 
     $cats = array();
     for($j = 0; $j < 35; $j++) {
@@ -26,14 +27,26 @@ $app->get('/fake', function () use ($app) {
         $post->title = $faker->catchPhrase;
         $post->link = str_replace(' ', '-', strtolower($post->title));
         $post->date = $faker->dateTime;
+        $post->kudos = rand(0, 100);
         $post->text = $faker->text;
         $author->ownPost[] = $post;
 
         R::tag($post, array($cats[$first], $cats[$second]));
 
-        R::store($post);
+        $comments = rand(0, 35);
+        for($j = 0; $j < $comments; $j++) {
+            $comment = R::dispense('comment');
+            $comment->author = $faker->name;
+            $comment->email = $faker->email;
+            $comment->gravatar = md5($comment->email);
+            $comment->website = (rand(0, 1) == 0) ? $faker->url : '';
+            $comment->comment = $faker->text(rand(5, 800));
+            $comment->time = $faker->unixTime;
+
+            $post->ownComment[] = $comment;
+        }
     }
 
-    //for($k = 0; $k < 1800; $k++) {
-    //}
+    R::store($author);
+    */
 });
