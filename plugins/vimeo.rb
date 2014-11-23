6350c0a026a5b86f41498925ff18c78de6ac3b80
lkorth <luke@lukekorth.com>
2014-11-22 22:22:30 -0800

    Move to Vimeo for videos

---
+++ plugins/vimeo.rb
@@ -0,0 +1,17 @@
+# Custom plugin from https://github.com/optikfluffel/octopress-responsive-video-embed
+
+module Jekyll
+  class Vimeo < Liquid::Tag
+
+    def initialize(name, id, tokens)
+      super
+      @id = id
+    end
+
+    def render(context)
+      %(<div class="embed-video-container"><iframe src="//player.vimeo.com/video/#{@id}"></iframe></div>)
+    end
+  end
+end
+
+Liquid::Template.register_tag('vimeo', Jekyll::Vimeo)
