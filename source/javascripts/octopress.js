c43ddcacbfc98703e1ed9316a428331da52b4007
lkorth <luke@lukekorth.com>
2014-11-24 22:21:11 -0800

    Add responsive nav, remove sticky nav

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -1,17 +1,3 @@
-function getNav() {
-  var mainNav = $('ul.main-navigation, ul[role=main-navigation]').before('<fieldset class="mobile-nav">')
-  var mobileNav = $('fieldset.mobile-nav').append('<select>');
-  mobileNav.find('select').append('<option value="">Navigate&hellip;</option>');
-  var addOption = function(i, option) {
-    mobileNav.find('select').append('<option value="' + this.href + '">&raquo; ' + $(this).text() + '</option>');
-  }
-  mainNav.find('a').each(addOption);
-  $('ul.subscription a').each(addOption);
-  mobileNav.find('select').bind('change', function(event) {
-    if (event.target.value) { window.location.href = event.target.value; }
-  });
-}
-
 function addCodeLineNumbers() {
   if (navigator.appName === 'Microsoft Internet Explorer') { return; }
   $('div.gist-highlight').each(function(code) {
@@ -30,27 +16,7 @@ function addCodeLineNumbers() {
 
 $('document').ready(function() {
   addCodeLineNumbers();
-  getNav();
-
-  var stickyNavTop = $('#nav').offset().top;
-  var nonFixedPadding = parseInt($('nav').css('paddingLeft'));
-  var bodyMargin = parseInt($('body').css('marginLeft'));
-
-  var stickyNav = function() {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > stickyNavTop) {
-      $('#nav-placeholder').css({display: 'block'});
-      $('#nav').addClass('sticky');
-      $('#nav').css({paddingLeft: nonFixedPadding + bodyMargin + 'px'});
-    } else {
-      $('#nav-placeholder').css({display: 'none'});
-      $('#nav').removeClass('sticky');
-      $('#nav').css({paddingLeft: nonFixedPadding + 'px'});
-    }
-  };
-
-  stickyNav();
-  $(window).scroll(stickyNav);
+  responsiveNav(".main-navigation");
 });
 
 // iOS scaling bug fix
